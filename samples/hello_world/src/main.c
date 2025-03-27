87d945f8f38828057a8dad1b9a5fe52c4a61348d
Daniel Oliveira <drawnpoetry@gmail.com>
2025-03-27 16:14:30 +0000

    ref(bao): change helloworld app to accomodate ipc
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -5,10 +5,47 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <zephyr/irq.h>
+#include <cmsis_core.h>
+
+#define IPC_IRQ_ID          63      //79 on Bao config
+#define BAO_IMAGE_START     0x10000000UL
+#define BAO_HC_OFF          0x41UL
+#define BAO_HC_ADDR         ((uintptr_t)BAO_IMAGE_START + BAO_HC_OFF)
+#define BAO_HC_IPC_ID       0x1
+#define VMS_IPC_BASE        0x20017000UL
+#define VMS_IPC_SIZE        0x1000
+
+void (*bao_hypercall)(unsigned int, unsigned int, unsigned int) =
+    (void (*)(unsigned int, unsigned int, unsigned int))BAO_HC_ADDR;
+
+char* const message1 = (char*)VMS_IPC_BASE;
+const size_t shmem_channel_size = VMS_IPC_SIZE/2;
+
+void ipc_notify(int ipc_id, int event_id)
+{
+    bao_hypercall(BAO_HC_IPC_ID, ipc_id, event_id);
+}
+
+void ipc_irq_handler(void)
+{
+    static unsigned int counter = 0;
+
+    printf("Writing to shared memory...\n");
+    sprintf(message1, "Counter: %d\r\n",++counter);
+    ipc_notify(0, 0);
+}
 
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+    printf("VM1 (Zephyr): Hello World! %s\n", CONFIG_BOARD_TARGET);
 
-	return 0;
+    memset(message1, 0, shmem_channel_size);
+
+    IRQ_CONNECT(IPC_IRQ_ID, 0, ipc_irq_handler, NULL, 0);
+    irq_enable(IPC_IRQ_ID);
+
+    while(1);
 }
